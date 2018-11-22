a57d2e59c81895977290489fd6997cbde02404b1
Alex Yu <alexjy@yahoo.com>
2018-11-22 01:27:20 -0500

    Update cnn-keras.py

--- cnn-keras.py
+++ cnn-keras.py
@@ -33,7 +33,11 @@ model.add(Dense(10, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Fit the model to the training data
-model.fit(x_train, y_train, batch_size=128, epochs=6, verbose=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, 
+          batch_size = 128, 
+          epochs = 6, 
+          verbose = 1, 
+          validation_data = (x_test, y_test))
 
 # Evaluate the accuracy of the model
 score = model.evaluate(x_test, y_test, verbose = 0)
