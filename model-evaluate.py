dc5c6e0ac18e3f46e62ce35e820c6ef7c5950be9
Alex Yu <alexjy@yahoo.com>
2018-11-22 01:26:04 -0500

    Update model-evaluate.py

--- model-evaluate.py
+++ model-evaluate.py
@@ -1,6 +1,10 @@
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=128, epochs=6, verbose=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, 
+          batch_size = 128, 
+          epochs = 6, 
+          verbose = 1, 
+          validation_data = (x_test, y_test))
 
 score = model.evaluate(x_test, y_test, verbose = 0)
 print('Test accuracy', score[1])
